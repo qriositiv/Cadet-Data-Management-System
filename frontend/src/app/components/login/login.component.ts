26ae56cb41b43cd59e61c5c864a6d99b9300f174
Arsenij <arsenij.nikulin@mif.stud.vu.lt>
2024-11-13 16:52:34 +0200

    Login component added reactive forms

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -1,20 +1,27 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './login.component.html'
 })
 export class LoginComponent {
-  cadetIdentificationNumber: string = '';
-  nationalIdentificationNumber: string = '';
+  loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      cadetIdentificationNumber: ['', Validators.required],
+      nationalIdentificationNumber: ['', Validators.required]
+    });
+  }
 
   onSubmit() {
-    if (this.cadetIdentificationNumber && this.nationalIdentificationNumber) {
-      console.log('Cadet ID:', this.cadetIdentificationNumber);
-      console.log('nationalIdentificationNumber:', this.nationalIdentificationNumber);
+    if (this.loginForm.valid) {
+      console.log('cadetIdentificationNumber:', this.loginForm.value.cadetIdentificationNumber);
+      console.log('nationalIdentificationNumber:', this.loginForm.value.nationalIdentificationNumber);
     } else {
       console.error('Both fields are required.');
     }
