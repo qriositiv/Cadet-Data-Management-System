fe13ecc1346974a9ead141089cfe2b8ae151b174
Arsenij <arsenij.nikulin@mif.stud.vu.lt>
2024-11-13 15:00:19 +0200

    Profile component created

---
+++ frontend/src/app/components/profile/profile.component.ts
@@ -0,0 +1,34 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profile',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './profile.component.html'
+})
+export class ProfileComponent {
+  currentTab: string = 'basic';
+
+  cadet = {
+    photoUrl: 'https://avatars.githubusercontent.com/u/116667154?v=4&size=64',
+    cadetId: 'VIL123456',
+    name: 'Arsenij Nikulin',
+    dateOfBirth: '2003 - 12 - 26',
+    civilId: '5031226****',
+    phone: '+37063013123',
+    email: 'qriositiv@gmail.com',
+    address: 'Baltijos pr. XX / XX, Klaipėda, Klaipėdos apsk.',
+    bloodType: 'O+',
+    height: 180.0,
+    weight: 75.7, 
+    allergies: 'Žuvys, riešutai',
+    medicalConditions: 'Astma'
+  };
+
+  constructor() { }
+
+  selectTab(tab: string) {
+    this.currentTab = tab;
+  }
+}
