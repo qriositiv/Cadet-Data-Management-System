4f62353c94351c29e87a4ed87b8a15eacd32e394
Arsenij <arsenij.nikulin@mif.stud.vu.lt>
2024-11-18 17:47:24 +0200

    Responsive design

--- frontend/src/app/components/header/header.component.ts
+++ frontend/src/app/components/header/header.component.ts
@@ -1,12 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './header.component.html'
 })
 export class HeaderComponent {
+  mobileMenuOpen: boolean = false;
+  
+  toggleMobileMenu() {
+    this.mobileMenuOpen = !this.mobileMenuOpen;
+  }
 
+  closeMobileMenu() {
+    this.mobileMenuOpen = false;
+  }
 }
